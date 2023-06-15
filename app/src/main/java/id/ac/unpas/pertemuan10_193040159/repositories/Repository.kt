a5f0a16e974f97b050935b84b7a8a71de59feb37
Fajrikh01 <fajrikh01@gmail.com>
2023-06-15 22:34:45 +0700

    Pertemuan 10

---
+++ app/src/main/java/id/ac/unpas/pertemuan10_193040159/repositories/Repository.kt
@@ -0,0 +1,4 @@
+package id.ac.unpas.pertemuan10_193040159.repositories
+
+interface Repository {
+}
---
+++ app/src/main/java/id/ac/unpas/pertemuan10_193040159/screens/SettingScreen.kt
@@ -0,0 +1,12 @@
+package id.ac.unpas.pertemuan10_193040159.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingScreen() {
+    Column {
+        Text(text = "Setting")
+    }
+}
